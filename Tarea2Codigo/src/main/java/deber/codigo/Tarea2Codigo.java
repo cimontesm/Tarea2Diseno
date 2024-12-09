7a217b875c3eb6c9f01f0faba04bfcd5db853cec
cimontesm <cimontes@espol.edu.ec>
2024-12-08 21:57:21 -0500

    Creacion del deber

---
+++ Tarea2Codigo/src/main/java/deber/codigo/Tarea2Codigo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deber.codigo;
+
+/**
+ *
+ * @author CMONTES
+ */
+public class Tarea2Codigo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
