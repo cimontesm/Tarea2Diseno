4bf886b9b0c24049758e2350b5242020b21ec4bc
GenesisMichilena <121325994+GenesisMichilena@users.noreply.github.com>
2025-01-27 22:39:24 -0500

    Remove Comments

--- Tarea2Codigo/src/main/java/deber/codigo/NotificationCenter.java
+++ Tarea2Codigo/src/main/java/deber/codigo/NotificationCenter.java
@@ -12,20 +12,17 @@ import java.util.List;
  * @author CMONTES
  */
 public class NotificationCenter {
-    
     private List<User> users = new ArrayList<>();
     
-    //se puede agregar un usuario para que reciba notificaciones
+
     public void addSubscriber(User user) {
         users.add(user);
     }
     
-    //se puede eliminar un usario para que ya no reciba notificaciones
     public void deleteSubscriber(User user) {
         users.remove(user);
     }
     
-    //en este se notifica al usuario subscrito
     public void notifySubscriber(String message) {
         for (User user : users) {
             user.update(message);
